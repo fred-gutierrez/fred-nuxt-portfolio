b93a4f758edff995f7c87bf881d525a518ddcbee
fred-gutierrez <fredsg22@hotmail.com>
2023-12-02 14:21:06 -0600

    Added basic SEO and a new icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "Fred David Solís Gutiérrez | Software Developer",
+      meta: [
+        {
+          name: "Fred David Solís Gutiérrez | Software Developer",
+          content:
+            "This is Fred David Solis Gutierrez's portfolio, where he showcases various projects and the skills he has acquired.",
+        },
+      ],
+      link: [{ rel: "icon", type: "image/png", href: "/logo-fred.png" }],
+    },
+  },
   devtools: { enabled: true },
   srcDir: "src/",
   css: ["@/assets/css/main.css"],
