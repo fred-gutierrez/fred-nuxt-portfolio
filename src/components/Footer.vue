2bc30b2a3eb01c6a5267f87c56a3973f5860c9a6
fred-gutierrez <fredsg22@hotmail.com>
2024-01-23 16:11:58 -0600

    Added the git repo link at the end of the Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,17 +2,14 @@
   <footer class="text-center py-10">
     <p class="inline-flex items-center dark:text-neutral-300 text-neutral-800">
       Made with
-      <img
-        src="@/assets/images/skills/frontend/nuxt-logo.png"
-        alt="Nuxt"
-        class="h-4 mx-1"
-      />
+      <img src="@/assets/images/skills/frontend/nuxt-logo.png" alt="Nuxt" class="h-4 mx-1" />
       and
-      <img
-        src="@/assets/images/skills/frontend/vuelogo.png"
-        alt="Vue"
-        class="h-4 mx-1"
-      /> by Fred
+      <img src="@/assets/images/skills/frontend/vuelogo.png" alt="Vue" class="h-4 mx-1" /> by Fred
+    </p>
+    <p class="dark:text-neutral-500 text-neutral-400 ">
+      <a class="hover:underline" href="https://github.com/fred-gutierrez/fred-nuxt-portfolio" target="_blank">
+        Repo <i class="fa-light fa-arrow-up-right-from-square fa-xs"></i>
+      </a>
     </p>
   </footer>
 </template>
